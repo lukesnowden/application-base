4de4bb6c21a4a1f35a20d118f006d5bc732e947d
Luke Snowden <luke@purposemedia.co.uk>
2014-09-07 02:38:26 +0100

    changed paths

--- app/views/Front/Globals/HTMLheader.blade.php
+++ app/views/Front/Globals/HTMLheader.blade.php
@@ -5,10 +5,10 @@
 		<title>Base Application</title>
 		<meta name="description" content="" />
 		<link href="//netdna.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
-		<link rel="stylesheet" href="/assets/css/style.css">
+		<link rel="stylesheet" href="/css/style.min.css">
 		<script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
 		<script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
-		<script src="/assets/js/app.js"></script>
+		<script src="/js/bundle.min.js"></script>
 	</head>
 	<body>
 
